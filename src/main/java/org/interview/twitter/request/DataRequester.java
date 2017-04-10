00122b99636a0d76b2a6b37d14c424b34a2050aa
Volodymyr_Arseienko <vovius@gmail.com>
2017-04-10 17:47:59 +0200

    implemented DataReader

--- src/main/java/org/interview/twitter/request/DataRequester.java
+++ src/main/java/org/interview/twitter/request/DataRequester.java
@@ -1,16 +1,21 @@
 package org.interview.twitter.request;
 
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.*;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Created by Volodymyr_Arseienko on 10.04.2017.
@@ -18,8 +23,16 @@ import java.nio.charset.StandardCharsets;
 @Component
 public class DataRequester {
 
+    private final static Logger LOG = LoggerFactory.getLogger(DataRequester.class);
+
     private final String requestURL;
     private HttpRequestFactory requestFactory;
+
+    @Value("${retrievePeriod}")
+    private int retrievePeriod;
+
+    @Value("${maxMessages}")
+    private int maxMessages;
 
     public DataRequester(String requestURL) {
         this.requestURL = requestURL;
@@ -29,17 +42,61 @@ public class DataRequester {
         this.requestFactory = requestFactory;
     }
 
-    public void request() {
+    public List<String> request() {
         GenericUrl url = new GenericUrl(requestURL);
         try {
             HttpRequest request = requestFactory.buildPostRequest(url, null);
-            HttpResponse response = request.execute();
+            final HttpResponse response = request.execute();
 
-            String result = IOUtils.toString(response.getContent());
-            System.out.println(result);
+            if (response.getStatusCode() == HttpStatusCodes.STATUS_CODE_OK) {
+                List<String> resultMessages = new LinkedList<>();
+
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+                final InputStream inputStream = response.getContent();
+                final Future task = executorService.submit(() -> {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    int curCnt = 0;
+                    while (curCnt < maxMessages) {
+                        String line = null;
+                        try {
+                            line = reader.readLine();
+                        } catch (IOException e) {
+                            LOG.error("Error when reading message, curCnt=" + curCnt);
+                        }
+
+                        if (line != null) {
+                            resultMessages.add(line);
+                            curCnt++;
+                        }
+
+                        LOG.debug(line);
+                    }
+
+                });
+
+                try {
+                    task.get(retrievePeriod, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    LOG.debug("Interrupted!", e);
+                } catch (ExecutionException e) {
+                    LOG.error("Exception during execution: ", e);
+                } catch (TimeoutException e) {
+                    LOG.info("Expired by timeout, messages read: " + resultMessages.size());
+                }
+
+                return resultMessages;
+
+            } else {
+                LOG.error("Failed to create Twitter streaming connection, error code = " + response.getStatusCode());
+            }
+
             response.disconnect();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Failed to execute request to Twitter: ", e);
         }
+
+        return null;
+
     }
+
 }
